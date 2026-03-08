535796f60ca72b12bde4311ffc0985dfff515095
nyaswabuhh <alex.navitech@gmail.com>
2026-03-08 11:00:29 +0300

    CORS

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -26,6 +26,9 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:5173", 
         "http://127.0.0.1:3000",
+        "https://apisimba.com",
+        "https://www.apisimba.com",
+    
     ]
     
     class Config:
